0040eef689febcaf6e19bd0b032b508b15c9b549
pB <pavelb654@gmail.com>
2016-05-01 14:23:04 +0300

    Got to the point where a total rewrite could come in handy

--- client/editor.js
+++ client/editor.js
@@ -1,5 +1,57 @@
+
+String.prototype.format = function() {
+    var formatted = this;
+    for (var arg in arguments) {
+        formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+    }
+    return formatted;
+}
+
+TILE_SPRITE_PATH = ["./resources/air.png", "./resources/solid/solid{0}.png", "./resources/destro/destro{0}.png"]
+
+// Tile sprite bitmask cheat sheet:
+// 8 - has connection up
+// 4 - has connection down
+// 2 - has connection left
+// 1 - has connection right
+
+tilespritesready = TILE_SPRITE_PATH.length * 16;
+tilespritesnow = 0;
+tilesprites = []
+
+for (var tile in TILE_SPRITE_PATH)
+{
+	tilesprites[tile] = []
+	for (var i = 0; i < 16; i++)
+	{
+		tilesprites[tile][i] = new Image();
+		tilesprites[tile][i].onload = function()
+		{
+			tilespritesnow++;
+		};
+		tilesprites[tile][i].src = TILE_SPRITE_PATH[tile].format(i);
+	}
+}
+
 Level = function(width, height)
 {
+	this.map = [];
+	for (var y = 0; y < height; y++)
+	{
+		this.map[y] = [];
+		for (var x = 0; x < width; x++)
+		{
+			this.map[y][x] = 0;
+		}
+	}
+
+	this.draw = function()
+	{
+		for (var y = 0; y < height; y++)
+		{
+			
+		}
+	}
 	//Stub :c
 }
 
