70225ab661be8ca5bf77baab3b9364a4e0e6d25e
pB <pavelb654@gmail.com>
2016-05-12 23:19:20 +0300

    Started doing things in regards to the server

---
+++ server/games/baseGame.py
@@ -0,0 +1,15 @@
+class GameInput:
+	def __init__(packet):
+		"""Process the packet and create an input object"""
+		# Stub
+		pass
+class Game:
+	def do_tick(user_inputs):
+		"""Do a single game tick and return a response.
+		
+		Arguments:
+		user_inputs -- a dictionary of player ID - GameInput pairs
+		
+		"""
+		# TODO actually plan this
+		pass
