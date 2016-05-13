ae577d13b54dbfa2e3e1081f01f5dff7d4ca81e3
pB <pavelb654@gmail.com>
2016-05-14 00:02:32 +0300

    Started adding lost of enterprise-code for stuff

---
+++ server/communication.py
@@ -0,0 +1,13 @@
+class GameInput:
+	def __init__(packet):
+		"""Process the packet and create an input object"""
+		# Stub
+		pass
+
+class GameOutput:
+
+	def set_screenshake(intensity):
+		self.screenshake = intensity
+
+	def act(entity, action):
+		pass
--- server/games/baseGame.py
+++ server/games/baseGame.py
@@ -1,15 +1,21 @@
-class GameInput:
-	def __init__(packet):
-		"""Process the packet and create an input object"""
-		# Stub
-		pass
+from server.communication import *
 class Game:
-	def do_tick(user_inputs):
+
+	class InvalidInputException(Exception):
+		pass
+
+	def do_tick(self, user_inputs):
 		"""Do a single game tick and return a response.
 		
 		Arguments:
 		user_inputs -- a dictionary of player ID - GameInput pairs
 		
+		Output:
+		A dictionary of player ID - GameResponse pairs to be returned to the corresponding users
 		"""
-		# TODO actually plan this
+
+		for user_id in user_inputs:
+			if not isinstance(user_inputs[user_id], GameInput):
+				raise self.InvalidInputException('Input of {user} is not a valid GameInput instance'.format(user=user_id))
 		pass
+
